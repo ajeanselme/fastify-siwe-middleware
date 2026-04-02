62a8d71febfd890b9e316a8d34e9c9c013f036d8
Antoine Jeanselme <67123340+ajeanselme@users.noreply.github.com>
2026-04-02 09:39:50 +0200

    Base app setup

---
+++ src/app.ts
@@ -0,0 +1,18 @@
+import fastify from "fastify";
+
+const app = fastify({ logger: true });
+
+app.get("/", async (request, reply) => {
+  return { hello: "world" };
+});
+
+const start = async () => {
+  try {
+    await app.listen({ port: 3000 });
+  } catch (err) {
+    app.log.error(err);
+    process.exit(1);
+  }
+};
+
+start();
